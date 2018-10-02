0bb702685a3c5b941e8e8c19a135e239ed7457d7
Kopytko10 <kopytko101930@wp.pl>
2018-10-02 12:35:05 +0200

    ćwiczenia z pętli for i while

--- python/petle-cw2.py
+++ python/petle-cw2.py
@@ -2,14 +2,24 @@
 # -*- coding: utf-8 -*-
 #
 #  petle-cw2.py
- 
+ # dwie dodatnie liczby podane przez użytkownika 
+ #dane wyjściowe- liczby naturalne z przedziału <n,m>
 
 
 def main(args):
+    
     n =int(input("Podaj liczbę: "))
+    while n < 1:
+        print("Błędne dane!")
+        n =int(input("Podaj liczbę: ")) 
+       
     m =int(input("Podaj liczbę: "))
+    while m <= n:
+        print("Błędne dane!")
+        m =int(input("Podaj liczbę: "))       
     for liczba in range(n, m + 1):
-        print(liczba, " ", end = " ")
+        
+        print(liczba, " ", end ="")
     
     return 0
     
