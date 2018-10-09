01e6778efcb5300ca3d3b887c98476e4b5d88664
Kopytko10 <kopytko101930@wp.pl>
2018-10-09 12:35:33 +0200

    prostokąty i choinka

--- python/figury.py
+++ python/figury.py
@@ -1,31 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 #  figury.py
+
+
 def prostokat(a, b, znak): 
     for i in range(a): #petla zewnetrzna
         for j in range(b): #petla wewnetrzna
             print(znak, end='') 
         print()  #znak końca lini-przejście do następnego wiersza 
     
-def prostokat2(a, b, znak):
+
+def prostokat2(a, b, znak, znak2):
     for i in range(a):
         for j in range(b):
-            if j == 0 or j = b - 1:
+            if j == 0 or j == b - 1 or i == 0 or i == a - 1:
                 print(znak, end='')
             else:
-                print(" ", end='')
+                print(znak2, end='')
         print()        
         
+
+def choinka(h, znak):
+    for i in range(h):
+        for j in range(h):
+            if j == 0 or j == h - 1:
+                print(znak, end='')
+                
+            else: 
+                print(end='')
+
 def main(args):
     
     a =int(input("Podaj długość boku: "))
     b =int(input("Podaj długość 2 boku: "))
     znak =(input("Podaj znak: "))
+    znak2 =(input("Podaj znak: "))
+    h =int(input("Podaj wysokość choinki: "))
     
     prostokat(a, b ,znak)
     print(" ")
-    prostokat2(a, b, znak)
+    print(" ")
+    prostokat2(a, b, znak, znak2)
+    print(" ")
+    print(" ")
+    choinka(h, znak)
     
     return 0
 
