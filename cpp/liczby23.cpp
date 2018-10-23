4cc72606994bcca7d8874aa75900ec8ebe09b882
Kopytko10 <kopytko101930@wp.pl>
2018-10-23 22:38:49 +0200

    liczby 3 cyfrowe cpp

--- cpp/liczby23.cpp
+++ cpp/liczby23.cpp
@@ -21,10 +21,25 @@ int liczby2() {
     return ile;
 } 
 
+int liczby3() {
+    int ile = 0
+    for (int i = 1; i < 10; i++) {
+        for (int j = 1; j < 10; j++) {
+            for (int k = 1; k < 10; k++){
+                if (i != j != k =! i){
+                    cout << i <<j << " ";
+                    ile++;
+                }
+            }
+        }
+    }
+    return ile;
+}
+
 int main(int argc, char **argv)
 {
 	cout << "\n\nLiczb 2-cyfrowych: " << liczby2();
-    
+    cout << "\n\nLiczb 3-cyfrowych: " << liczby3();
     
 	return 0;
 }
