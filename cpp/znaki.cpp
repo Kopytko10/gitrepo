6f2586ac0e5eba3a8111b124a4cf71505270f168
Kopytko10 <kopytko101930@wp.pl>
2018-11-07 08:45:37 +0100

    tablice

---
+++ cpp/znaki.cpp
@@ -0,0 +1,50 @@
+/*
+ * znaki.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int zlicz(char tab[]){
+    int i = 0; // indeks znaków w tablicy
+    while (tab[i] != '\0') i++;
+    return i;
+    
+}
+
+void drukuj(char tab[],int rozmiar) {
+    for(int i=0; i < rozmiar; i++) {
+        cout << tab[i] << " ";
+    }
+}
+
+void liczznaki(char tab[], int roz) {
+    int spacje = 0;
+    int interpunkcja = 0;
+    int symbole = 0
+    for(int i=0; i < rozmiar; i++) {
+        if (tab[i] == ' ') spacje++;
+        else if (tab[i] == '.' || tab[i] == ',') interpunkcja++;
+        else if (tab[i] == '(' || tab[i] == ')') symbole++;
+    }
+}
+    
+    
+
+int main(int argc, char **argv)
+{
+	const int rozmiar = 20;
+    char tab[rozmiar];
+    cout << "Jak się nazywasz?" << endl;
+    //cin >> tab;
+    cin.getline(tab, rozmiar);
+    //cout << "Cześć " << tab << "!" << endl;
+    drukuj(tab, zlicz(tab));
+    
+    
+    
+	return 0;
+}
+
