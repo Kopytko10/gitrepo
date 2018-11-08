c0556b14a987b6eee5169ae175b4372f0a36b1be
Kopytko10 <kopytko101930@wp.pl>
2018-11-08 07:56:32 +0100

    ascii małe duże

--- cpp/znaki.cpp
+++ cpp/znaki.cpp
@@ -20,15 +20,50 @@ void drukuj(char tab[],int rozmiar) {
     }
 }
 
-void liczznaki(char tab[], int roz) {
+void ascii(char tab[],int rozmiar) {
+    int kod = 0; // kod ascii znaku
+    for(int i=0; i < rozmiar; i++) {
+        kod = (int)tab[i];
+        if (kod > 65 && kod < 90)
+            cout << (char)(kod+32)  << " ";
+        if (kod > 96 && kod < 123)
+            cout << (char)(kod-32)  << " ";
+        else
+            cout << (int)tab[i] << " ";
+       
+    }
+}
+
+void liczznaki(char tab[], int rozmiar) {
     int spacje = 0;
     int interpunkcja = 0;
-    int symbole = 0
+    int symbole = 0;
+    int reszta = 0;
     for(int i=0; i < rozmiar; i++) {
-        if (tab[i] == ' ') spacje++;
-        else if (tab[i] == '.' || tab[i] == ',') interpunkcja++;
-        else if (tab[i] == '(' || tab[i] == ')') symbole++;
+        //~if (tab[i] == ' ') spacje++;
+        //~else if (tab[i] == '.' || tab[i] == ',') interpunkcja++;
+        //~else if (tab[i] == '(' || tab[i] == ')') symbole++;
+        switch (tab[i]) {
+            case ' ':
+            case '\t':
+                spacje++;
+            break;
+            case '.':
+            case ',':
+                interpunkcja++;
+            break;
+            case '(':
+            case ')':
+                symbole++;
+            break;   
+            default:
+                reszta++;
+        }
     }
+    cout << "Spacje: " << spacje << endl;
+    cout << "interpunkcja: " << interpunkcja << endl;
+    cout << "symbole: " << symbole << endl;
+    cout << "reszta: " << reszta << endl;
 }
     
     
@@ -41,10 +76,9 @@ int main(int argc, char **argv)
     //cin >> tab;
     cin.getline(tab, rozmiar);
     //cout << "Cześć " << tab << "!" << endl;
-    drukuj(tab, zlicz(tab));
-    
-    
-    
+    //drukuj(tab, zlicz(tab));
+    ascii(tab, zlicz(tab));
+    liczznaki(tab, zlicz(tab));
 	return 0;
 }
 
