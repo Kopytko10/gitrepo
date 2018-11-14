00de6ec6960f8143e87966de83080da1a6e96fb4
Kopytko10 <kopytko101930@wp.pl>
2018-11-14 08:19:01 +0100

    anaggramy

--- cpp/anagramy.cpp
+++ cpp/anagramy.cpp
@@ -12,10 +12,13 @@ void anagramy(char w[], int r) {
     i1 = i2 = i3 = i4 = 0;
     for (i1 = 0; i1 < r; i1++) {
         for (i2 = 0; i2 < r; i2++){
+            if(i1 == i2) continue;
             for (i3 = 0; i3 < r; i3++){
-                for (i4 = 0; i4 < r; i4++)
-            if(i1 == i2 or i2 == i3 or i1 == i3 or i1 == i4 or i2 == i4) continue;
-            cout << i1 << i2 << endl;
+                if(i1 == i3 || i2 == i3) continue;
+                //for (i4 = 0; i4 < r; i4++)
+                //if( i1 == i4 || i2 == i4 || i3 == i4) continue;
+                i4 = 6 - (i1 + i2 + i3);
+            cout << i1 << i2 << i3 << i4 << endl;
             }
         }
     }
