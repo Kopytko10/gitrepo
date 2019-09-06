636a6176d89478a5bd0ed5c62e116d70d2a55375
Kopytko10 <kopytko101930@wp.pl>
2019-09-06 08:47:10 +0200

    Przypomnienie

---
+++ cpp/wej-wyj.cpp
@@ -0,0 +1,20 @@
+/*
+ * wej-wyj.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    string n;
+    string m;
+        cout << "Podaj imię: ";
+        cin >> n;
+            cout << "Podaj nazwisko: ";
+            cin >> m;
+                cout << "Witaj "<< n << " " << m << endl;
+	return 0;
+}
+
